4188449b4b41f53d0e85d2197178c34ce56d8e03
Deivison isidoro <deivisonisidoro@gmail.com>
2024-08-28 19:37:12 -0300

    feat: creating abstract measure repository

---
+++ src/domain/repositories/abstract.measure.repository.ts
@@ -0,0 +1,44 @@
+import { Measure } from '@prisma/client';
+import { UploadMeasureDto } from '../dto/upload-measure.dto';
+
+
+/**
+ * Abstract class defining the core methods for measure repositories.
+ */
+export abstract class AbstractMeasureRepository {
+
+  /**
+   * Creates a new measurement.
+   * @param data - Data transfer object for uploading a measurement.
+   * @returns Promise of the created measurement.
+   */
+  abstract create(data: UploadMeasureDto): Promise<Measure>;
+
+  /**
+   * Finds all measurements.
+   * @returns Promise of all measurements.
+   */
+  abstract findAll(): Promise<Measure[]>;
+
+  /**
+   * Finds a measurement by its unique identifier.
+   * @param measure_uuid - The unique identifier for the measurement.
+   * @returns Promise of the measurement or null if not found.
+   */
+  abstract findOne(measure_uuid: string): Promise<Measure | null>;
+
+  /**
+   * Finds measurements by customer code.
+   * @param customer_code - The unique code identifying the customer.
+   * @returns Promise of measurements associated with the given customer code or null if not found.
+   */
+  abstract findByCustomerCode(customer_code: string): Promise<Measure[] | null>;
+
+  /**
+   * Updates a measurement.
+   * @param measure_uuid - The unique identifier for the measurement.
+   * @param data - Partial data to update the measurement.
+   * @returns Promise of the updated measurement.
+   */
+  abstract update(measure_uuid: string, data: Partial<Measure>): Promise<Measure>;
+}
