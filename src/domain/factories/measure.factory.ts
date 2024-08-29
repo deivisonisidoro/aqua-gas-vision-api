5af89ed20b9b8bb50cfe36793d29e8fe2e03157a
Deivison isidoro <deivisonisidoro@gmail.com>
2024-08-29 11:50:14 -0300

    fix: improving the create method doing it to validate data

--- src/domain/factories/measure.factory.ts
+++ src/domain/factories/measure.factory.ts
@@ -1,4 +1,5 @@
-import { IMeasureEntity, MeasureEntity } from '../entities/measure.entity';
+import { MeasureEntityType } from '../entities/@types/measure.entity.type';
+import { MeasureEntity } from '../entities/measure.entity';
 
 /**
  * Factory class for creating MeasureEntity instances.
@@ -7,9 +8,18 @@ export class MeasureFactory {
   /**
    * Creates a new MeasureEntity instance with the specified values.
    *
-   * @param {IMeasureEntity} props - The properties to initialize the MeasureEntity.
+   * @param {MeasureEntityType} props - The properties to initialize the MeasureEntity.
    */
-  static create(props: IMeasureEntity): MeasureEntity {
-    return new MeasureEntity(props);
+  static create(props: MeasureEntityType): MeasureEntity {
+    const measureEntity = new MeasureEntity({} as MeasureEntityType);
+
+    measureEntity.measure_datetime = props.measure_datetime;
+    measureEntity.measure_type = props.measure_type;
+    measureEntity.has_confirmed = props.has_confirmed;
+    measureEntity.image_url = props.image_url;
+    measureEntity.customer_code = props.customer_code;
+    measureEntity.measure_value = props.measure_value;
+
+    return measureEntity;
   }
 }
