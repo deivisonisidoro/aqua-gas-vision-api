72dcab1993acb187bb1d2f3e150a3dfbb7f4984b
Deivison isidoro <deivisonisidoro@gmail.com>
2024-08-29 11:51:30 -0300

    test: fixing tests

--- src/presentation/measure.controller.spec.ts
+++ src/presentation/measure.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MeasureController } from '../presentation/measure.controller';
 import { MeasureService } from '../application/services/measure.service';
+import { AbstractMeasureService } from '../domain/services/abstract.measure.service';
+import { AbstractMeasureRepository } from '../domain/repositories/abstract.measure.repository';
+import { MeasureRepository } from '../infrastructure/repositories/measure.repository';
+import { PrismaService } from '../infrastructure/database/prisma.service';
 
 describe('MeasureController', () => {
   let controller: MeasureController;
@@ -8,7 +12,17 @@ describe('MeasureController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MeasureController],
-      providers: [MeasureService],
+      providers: [
+        {
+          provide: AbstractMeasureService,
+          useClass: MeasureService,
+        },
+        {
+          provide: AbstractMeasureRepository,
+          useClass: MeasureRepository,
+        },
+        PrismaService,
+      ],
     }).compile();
 
     controller = module.get<MeasureController>(MeasureController);
