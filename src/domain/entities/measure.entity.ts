a83a74ac7183af9d0a35027b2c3d72c5f3f10161
Deivison isidoro <deivisonisidoro@gmail.com>
2024-08-30 18:34:58 -0300

    feat: adding new business role

--- src/domain/entities/measure.entity.ts
+++ src/domain/entities/measure.entity.ts
@@ -24,14 +24,15 @@ export class MeasureEntity implements MeasureEntityType {
    * @param {MeasureEntityType} props - The properties to initialize the MeasureEntity.
    */
   constructor(props: MeasureEntityType) {
-    this._measure_uuid = props.measure_uuid;
-    this._measure_datetime = props.measure_datetime;
-    this._measure_type = props.measure_type;
-    this._has_confirmed = props.has_confirmed;
-    this._image_url = props.image_url;
-    this._customer_code = props.customer_code;
-    this._measure_value = props.measure_value;
+    this.measure_uuid = props.measure_uuid;
+    this.measure_datetime = props.measure_datetime;
+    this.measure_type = props.measure_type;
+    this.has_confirmed = props.has_confirmed;
+    this.image_url = props.image_url;
+    this.customer_code = props.customer_code;
+    this.measure_value = props.measure_value;
   }
+
   /** @returns {string} The unique identifier for the measure. */
   get measure_uuid(): string {
     return this._measure_uuid;
@@ -49,6 +50,14 @@ export class MeasureEntity implements MeasureEntityType {
 
   /** @param {Date} value - The date and time when the measurement was taken. */
   set measure_datetime(value: Date) {
+    console.log(value);
+
+    if (!(value instanceof Date) || isNaN(value.getTime())) {
+      throw new BadRequestException(
+        ErrorMessagesMessageEnum.INVALID_DATETIME,
+        'INVALID_DATA',
+      );
+    }
     this._measure_datetime = value;
   }
 
@@ -62,6 +71,7 @@ export class MeasureEntity implements MeasureEntityType {
     if (value !== 'WATER' && value !== 'GAS') {
       throw new BadRequestException(
         ErrorMessagesMessageEnum.INVALID_MEASURE_TYPE,
+        'INVALID_DATA',
       );
     }
     this._measure_type = value;
@@ -74,6 +84,12 @@ export class MeasureEntity implements MeasureEntityType {
 
   /** @param {boolean} value - Indicates if the measurement has been confirmed. */
   set has_confirmed(value: boolean) {
+    if (typeof value !== 'boolean') {
+      throw new BadRequestException(
+        ErrorMessagesMessageEnum.INVALID_CONFIRMATION_STATUS,
+        'INVALID_DATA',
+      );
+    }
     this._has_confirmed = value;
   }
 
@@ -86,7 +102,10 @@ export class MeasureEntity implements MeasureEntityType {
   set image_url(value: string) {
     const base64Pattern = /^data:image\/[a-zA-Z]+;base64,/i;
     if (!base64Pattern.test(value)) {
-      throw new BadRequestException(ErrorMessagesMessageEnum.INVALID_IMAGE_URL);
+      throw new BadRequestException(
+        ErrorMessagesMessageEnum.INVALID_IMAGE_URL,
+        'INVALID_DATA',
+      );
     }
     this._image_url = value;
   }
@@ -98,6 +117,12 @@ export class MeasureEntity implements MeasureEntityType {
 
   /** @param {string} value - The unique code identifying the customer. */
   set customer_code(value: string) {
+    if (typeof value !== 'string' || value.trim() === '') {
+      throw new BadRequestException(
+        ErrorMessagesMessageEnum.INVALID_CUSTOMER_CODE,
+        'INVALID_DATA',
+      );
+    }
     this._customer_code = value;
   }
 
@@ -109,14 +134,16 @@ export class MeasureEntity implements MeasureEntityType {
   /** @param {number} value - The value of the measurement. */
   set measure_value(value: number) {
     if (value !== undefined) {
-      if (!Number.isInteger(value)) {
+      if (typeof value !== 'number' || !Number.isInteger(value)) {
         throw new BadRequestException(
           ErrorMessagesMessageEnum.INVALID_MEASURE_VALUE,
+          'INVALID_DATA',
         );
       }
       if (value < 0) {
         throw new BadRequestException(
           ErrorMessagesMessageEnum.INVALID_MEASURE_VALUE,
+          'INVALID_DATA',
         );
       }
     }
