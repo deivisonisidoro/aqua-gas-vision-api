de94829cca0aa8399fd61e87234f747a04da37af
Deivison isidoro <deivisonisidoro@gmail.com>
2024-08-30 19:23:56 -0300

    fixL removing validations

--- src/domain/dto/confirm-measure.dto.ts
+++ src/domain/dto/confirm-measure.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsUUID, IsInt, Min, IsOptional } from 'class-validator';
-import { ErrorMessagesMessageEnum } from '../enums/error.messages/message.enum';
+import { IsOptional } from 'class-validator';
 
 /**
  * DTO for confirming a measurement.
@@ -10,15 +9,12 @@ export class ConfirmMeasurementDto {
     description: 'The unique identifier for the measurement.',
     type: String,
   })
-  @IsUUID('4', { message: ErrorMessagesMessageEnum.INVALID_MEASURE_UUID })
   measure_uuid: string;
 
   @ApiProperty({
     description: 'The confirmed value of the measurement.',
     type: Number,
   })
-  @IsInt({ message: ErrorMessagesMessageEnum.INVALID_CONFIRMED_VALUE })
-  @Min(0, { message: ErrorMessagesMessageEnum.MIN_CONFIRMED_VALUE })
   confirmed_value: number;
 
   @ApiPropertyOptional({
