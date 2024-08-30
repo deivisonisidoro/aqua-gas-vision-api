3987678327c4a2ee65555cd2b8f58516cc5cd889
Deivison isidoro <deivisonisidoro@gmail.com>
2024-08-30 18:35:33 -0300

    fix: removing validations

--- src/domain/dto/upload-measure.dto.ts
+++ src/domain/dto/upload-measure.dto.ts
@@ -1,14 +1,4 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  IsString,
-  IsEnum,
-  IsDateString,
-  Matches,
-  IsOptional,
-  IsInt,
-  Min,
-} from 'class-validator';
-import { ErrorMessagesMessageEnum } from '../enums/error.messages/message.enum';
 
 /**
  * DTO for uploading a measurement.
@@ -18,17 +8,12 @@ export class UploadMeasureDto {
     description: 'The base64 encoded image representing the measurement.',
     type: String,
   })
-  @IsString({ message: ErrorMessagesMessageEnum.INVALID_IMAGE_URL })
-  @Matches(/^data:image\/[a-zA-Z]+;base64,/i, {
-    message: ErrorMessagesMessageEnum.INVALID_IMAGE_URL,
-  })
   image: string;
 
   @ApiProperty({
     description: 'The unique code identifying the customer.',
     type: String,
   })
-  @IsString({ message: ErrorMessagesMessageEnum.INVALID_CUSTOMER_CODE })
   customer_code: string;
 
   @ApiProperty({
@@ -36,18 +21,11 @@ export class UploadMeasureDto {
     type: String,
     format: 'date-time',
   })
-  @IsDateString(
-    {},
-    { message: ErrorMessagesMessageEnum.INVALID_MEASURE_DATETIME },
-  )
   measure_datetime: Date;
 
   @ApiProperty({
     description: "The type of measurement, either 'WATER' or 'GAS'.",
     enum: ['WATER', 'GAS'],
-  })
-  @IsEnum(['WATER', 'GAS'], {
-    message: ErrorMessagesMessageEnum.INVALID_MEASURE_TYPE,
   })
   measure_type: string;
 
@@ -55,8 +33,5 @@ export class UploadMeasureDto {
     description: 'The value of the measurement.',
     type: Number,
   })
-  @IsOptional()
-  @IsInt({ message: ErrorMessagesMessageEnum.INVALID_MEASURE_VALUE })
-  @Min(0, { message: ErrorMessagesMessageEnum.INVALID_MEASURE_VALUE })
   measure_value?: number;
 }
