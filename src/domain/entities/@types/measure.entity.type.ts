50e11ea1517e52c3498955a0478a889fb58762f4
Deivison isidoro <deivisonisidoro@gmail.com>
2024-08-30 18:36:00 -0300

    fix: transforming fields in optionals

--- src/domain/entities/@types/measure.entity.type.ts
+++ src/domain/entities/@types/measure.entity.type.ts
@@ -5,8 +5,8 @@ export type MeasureEntityType = {
   measure_uuid?: string;
   measure_datetime: Date;
   measure_type: string;
-  has_confirmed: boolean;
+  has_confirmed?: boolean;
   image_url: string;
   customer_code: string;
-  measure_value: number;
+  measure_value?: number;
 };
